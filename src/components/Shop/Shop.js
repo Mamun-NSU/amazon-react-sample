be2bb2abeee68899a28be8363fba594ed32ad04f
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-29 23:58:21 +0600

    shop component updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './Shop.css';
 
 const Shop = () => {
     return (
-        <div>
-
+        <div className="shop-container">
+            <div className="products-container">
+                <h2>This is products container</h2>
+            </div>
+            <div className="cart-container">
+                <h2>This is cart container</h2>
+            </div>
         </div>
     );
 };
